464f6317566691250e1e153f0dd66c95cd4a9b88
Alisher <Alisher>
2020-12-04 14:31:49 +0600

    added code to parse the json response

---
+++ youtube-app/Response.swift
@@ -0,0 +1,27 @@
+//
+//  Response.swift
+//  youtube-app
+//
+//  Created by Алишер Алсейт on 04.12.2020.
+//
+
+import Foundation
+
+struct Response : Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys:String, CodingKey {
+        
+        case items
+    }
+    
+    
+    init (from decoder:Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items =  try container.decode([Video].self, forKey: .items)
+    }
+    
+}
